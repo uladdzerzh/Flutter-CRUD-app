401152db99ca5afd48f98e860f38f3e906364762
buter <vderzhanovich@gmail.com>
2023-04-17 11:27:02 +0200

    Flutter app for politech

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Yego Form',
+      title: 'Uladzislau Dzerzhanovich',
       initialRoute: '/',
       routes: {
         '/': (context) => const YegoForm(),
